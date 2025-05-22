0d6506ca4cf7fa334d9ddf1e751ea37da5e18cc8
lmzapatat <lmzapatat@gmail.com>
2025-05-22 17:26:16 +0200

    New elements

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // script.js
 console.log("Website loaded successfully!");
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
